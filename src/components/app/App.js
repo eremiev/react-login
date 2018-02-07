4a8e906ef938fa4bebffe8a9acddc0cfa3290308
Emil Eremiev <eeremiev@MacBook-Pro-de-usuario-2.local>
2018-02-07 13:33:52 +0100

    make login

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -13,13 +13,16 @@ export default class App extends Component {
     constructor() {
         super();
         this.state = {
-            response: false,
+            logged: false,
             username: '',
-            fPrint: (new Fingerprint2()).getSync().fprint
+            fPrint: (new Fingerprint2()).getSync().fprint,
+            userList: {},
+            user: {}
         };
 
         this.onListening = this.onListening.bind(this);
         this.onCheckUser = this.onCheckUser.bind(this);
+        this.onUpdate = this.onUpdate.bind(this);
     }
 
     componentDidMount() {
@@ -28,22 +31,40 @@ export default class App extends Component {
 
     onListening() {
 
-        socket.on("login", data => {
-            console.log(data)
+        socket.on("login", loginData => {
+            this.onUpdate(loginData.logged, loginData.user, loginData.userList);
+            this.onPerformLogin();
         });
+    }
+
+    onUpdate(logged, user = null, userList = null) {
+        this.setState({logged, user, userList});
     }
 
     onCheckUser() {
         socket.emit('checkUserLogin', {deviceId: this.state.fPrint, username: this.state.username});
     }
 
+    onPerformLogin() {
+        if (!this.state.logged) {
+           //create new user
+            socket.emit('performLogin', {deviceId: this.state.fPrint, username: this.state.username});
+        }else{
+            //user already have acc.
+            // console.log(this.state.user, this.state.userList);
+        }
+    }
+
     render() {
-        const {response} = this.state;
+        const {logged, user} = this.state;
         return (
             <div style={{textAlign: "center"}}>
-                {response
+                {logged
                     ?
+                    <div>
+                        <h1>Wellcome, {user.username}</h1>
                     <p>Chat lists...</p>
+                    </div>
                     :
                     <Login onInputChange={username => this.setState({username})} checkUser={this.onCheckUser}/>
                 }
